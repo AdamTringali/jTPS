b5e8731f4439577d56f0da6d867525d72d92fe4d
AdamTringali <aatringali@gmail.com>
2019-10-15 00:24:32 -0400

    updated styling

--- jTPS-mine/Unit_Testing.js
+++ jTPS-mine/Unit_Testing.js
@@ -4,26 +4,26 @@
 
  function add5Button() {
     jsTpss.addTransaction(new AddToNum_Transaction(numb, 5));
-    document.getElementById("getNum5").innerHTML = document.getElementById("getNum5").innerHTML + " = " + numb.getNum();
-    document.getElementById("getSize5").innerHTML =  document.getElementById("getSize5").innerHTML + " = " + jsTpss.getSize();
-    document.getElementById("getRedoSize5").innerHTML = document.getElementById("getRedoSize5").innerHTML + " = " + jsTpss.getRedoSize();
-    document.getElementById("getUndoSize5").innerHTML =  document.getElementById("getUndoSize5").innerHTML + " = " + jsTpss.getUndoSize();
+    document.getElementById("getNum5").value =  numb.getNum();
+    document.getElementById("getSize5").value =   jsTpss.getSize();
+    document.getElementById("getRedoSize5").value =  jsTpss.getRedoSize();
+    document.getElementById("getUndoSize5").value =   jsTpss.getUndoSize();
 }
 
 function add10Button() {
     jsTpss.addTransaction(new AddToNum_Transaction(numb, 10));
-    document.getElementById("getNum10").innerHTML = document.getElementById("getNum10").innerHTML + " = " + numb.getNum();
-    document.getElementById("getSize10").innerHTML =  document.getElementById("getSize10").innerHTML + " = " + jsTpss.getSize();
-    document.getElementById("getRedoSize10").innerHTML = document.getElementById("getRedoSize10").innerHTML + " = " + jsTpss.getRedoSize();
-    document.getElementById("getUndoSize10").innerHTML =  document.getElementById("getUndoSize10").innerHTML + " = " + jsTpss.getUndoSize();
+    document.getElementById("getNum10").value =  numb.getNum();
+    document.getElementById("getSize10").value =  jsTpss.getSize();
+    document.getElementById("getRedoSize10").value =  jsTpss.getRedoSize();
+    document.getElementById("getUndoSize10").value =  jsTpss.getUndoSize();
 }
 
 function add20Button() {
     jsTpss.addTransaction(new AddToNum_Transaction(numb, 20));
-    document.getElementById("getNum20").innerHTML = document.getElementById("getNum20").innerHTML + " = " + numb.getNum();
-    document.getElementById("getSize20").innerHTML =  document.getElementById("getSize20").innerHTML + " = " + jsTpss.getSize();
-    document.getElementById("getRedoSize20").innerHTML = document.getElementById("getRedoSize20").innerHTML + " = " + jsTpss.getRedoSize();
-    document.getElementById("getUndoSize20").innerHTML =  document.getElementById("getUndoSize20").innerHTML + " = " + jsTpss.getUndoSize();
+    document.getElementById("getNum20").value =  numb.getNum();
+    document.getElementById("getSize20").value =   jsTpss.getSize();
+    document.getElementById("getRedoSize20").value =  jsTpss.getRedoSize();
+    document.getElementById("getUndoSize20").value =  jsTpss.getUndoSize();
 }
 
 function add12Button() {
@@ -31,65 +31,65 @@ function add12Button() {
     numb = new Num();
     jsTpss.addTransaction(new AddToNum_Transaction(numb, 12));
     jsTpss.addTransaction(new AndMask_Transaction(numb, numb.getNum(), 4));
-    document.getElementById("getNum12").innerHTML = document.getElementById("getNum12").innerHTML + " = " + numb.getNum();
-    document.getElementById("getSize12").innerHTML = document.getElementById("getSize12").innerHTML + " = " + jsTpss.getSize();
+    document.getElementById("getNum12").value = numb.getNum();
+    document.getElementById("getSize12").value = jsTpss.getSize();
 
     jsTpss.undoTransaction();
-    document.getElementById("getNum12undo").innerHTML +=  " = " + numb.getNum();
-    document.getElementById("getSize12undo").innerHTML +=  " = " + jsTpss.getSize();
-    document.getElementById("getRedoSize12undo").innerHTML +=  " = " + jsTpss.getRedoSize();
-    document.getElementById("getUndoSize12undo").innerHTML +=  " = " + jsTpss.getUndoSize();
+    document.getElementById("getNum12undo").value =  numb.getNum();
+    document.getElementById("getSize12undo").value =  jsTpss.getSize();
+    document.getElementById("getRedoSize12undo").value =  jsTpss.getRedoSize();
+    document.getElementById("getUndoSize12undo").value = jsTpss.getUndoSize();
 }
 
 function add3Button(){
     jsTpss = new jsTPS();
     numb = new Num();
-    document.getElementById("getNum3").innerHTML +=  " = " + numb.getNum();
-    document.getElementById("hasTransactionUndo").innerHTML +=  " = " + jsTpss.hasTransactionToUndo();
-    document.getElementById("hasTransactionRedo").innerHTML +=  " = " + jsTpss.hasTransactionToRedo();
+    document.getElementById("getNum3").value =  numb.getNum();
+    document.getElementById("hasTransactionUndo").value = jsTpss.hasTransactionToUndo();
+    document.getElementById("hasTransactionRedo").value = jsTpss.hasTransactionToRedo();
 
     jsTpss.addTransaction(new AddToNum_Transaction(numb, 5));
     jsTpss.addTransaction(new AddToNum_Transaction(numb, 10));
     jsTpss.addTransaction(new AddToNum_Transaction(numb, 20));
-    document.getElementById("num3").innerHTML +=  " = " + jsTpss.hasTransactionToUndo();
-    document.getElementById("num4").innerHTML +=  " = " + jsTpss.hasTransactionToRedo();
-    document.getElementById("num5").innerHTML +=  " = " + numb.getNum();
-    document.getElementById("num6").innerHTML +=  " = " + jsTpss.hasTransactionToUndo();
-    document.getElementById("num7").innerHTML +=  " = " + jsTpss.getSize();
-    document.getElementById("num8").innerHTML +=  " = " + jsTpss.getRedoSize();
-    document.getElementById("num9").innerHTML +=  " = " + jsTpss.getUndoSize();
+    document.getElementById("num3").value =  jsTpss.hasTransactionToUndo();
+    document.getElementById("num4").value =  jsTpss.hasTransactionToRedo();
+    document.getElementById("num5").value =  numb.getNum();
+    document.getElementById("num6").value =  jsTpss.hasTransactionToUndo();
+    document.getElementById("num7").value =  jsTpss.getSize();
+    document.getElementById("num8").value =  jsTpss.getRedoSize();
+    document.getElementById("num9").value =  jsTpss.getUndoSize();
 
     jsTpss.undoTransaction();
-    document.getElementById("num10").innerHTML +=  " = " + jsTpss.hasTransactionToUndo();
-    document.getElementById("num11").innerHTML +=  " = " + jsTpss.hasTransactionToRedo();
-    document.getElementById("num12").innerHTML +=  " = " + numb.getNum();
-    document.getElementById("num13").innerHTML +=  " = " + jsTpss.getSize();
-    document.getElementById("num14").innerHTML +=  " = " + jsTpss.getRedoSize();
-    document.getElementById("num15").innerHTML +=  " = " + jsTpss.getUndoSize();
+    document.getElementById("num10").value =  jsTpss.hasTransactionToUndo();
+    document.getElementById("num11").value =  jsTpss.hasTransactionToRedo();
+    document.getElementById("num12").value =  numb.getNum();
+    document.getElementById("num13").value =  jsTpss.getSize();
+    document.getElementById("num14").value =  jsTpss.getRedoSize();
+    document.getElementById("num15").value =  jsTpss.getUndoSize();
 
     jsTpss.undoTransaction();
-    document.getElementById("num16").innerHTML +=  " = " + jsTpss.hasTransactionToUndo();
-    document.getElementById("num17").innerHTML +=  " = " + jsTpss.hasTransactionToRedo();
-    document.getElementById("num18").innerHTML +=  " = " + numb.getNum();
-    document.getElementById("num19").innerHTML +=  " = " + jsTpss.getSize();
-    document.getElementById("num20").innerHTML +=  " = " + jsTpss.getRedoSize();
-    document.getElementById("num21").innerHTML +=  " = " + jsTpss.getUndoSize();
+    document.getElementById("num16").value =  jsTpss.hasTransactionToUndo();
+    document.getElementById("num17").value =  jsTpss.hasTransactionToRedo();
+    document.getElementById("num18").value =  numb.getNum();
+    document.getElementById("num19").value =  jsTpss.getSize();
+    document.getElementById("num20").value =  jsTpss.getRedoSize();
+    document.getElementById("num21").value =  jsTpss.getUndoSize();
 
     jsTpss.undoTransaction();
-    document.getElementById("num22").innerHTML +=  " = " + jsTpss.hasTransactionToUndo();
-    document.getElementById("num23").innerHTML +=  " = " + jsTpss.hasTransactionToRedo();
-    document.getElementById("num24").innerHTML +=  " = " + numb.getNum();
-    document.getElementById("num25").innerHTML +=  " = " + jsTpss.getSize();
-    document.getElementById("num26").innerHTML +=  " = " + jsTpss.getRedoSize();
-    document.getElementById("num27").innerHTML +=  " = " + jsTpss.getUndoSize();
+    document.getElementById("num22").value =  jsTpss.hasTransactionToUndo();
+    document.getElementById("num23").value =  jsTpss.hasTransactionToRedo();
+    document.getElementById("num24").value =  numb.getNum();
+    document.getElementById("num25").value =  jsTpss.getSize();
+    document.getElementById("num26").value =  jsTpss.getRedoSize();
+    document.getElementById("num27").value =  jsTpss.getUndoSize();
 
     jsTpss.undoTransaction();
-    document.getElementById("num28").innerHTML +=  " = " + jsTpss.hasTransactionToUndo();
-    document.getElementById("num29").innerHTML +=  " = " + jsTpss.hasTransactionToRedo();
-    document.getElementById("num30").innerHTML +=  " = " + numb.getNum();
-    document.getElementById("num31").innerHTML +=  " = " + jsTpss.getSize();
-    document.getElementById("num32").innerHTML +=  " = " + jsTpss.getRedoSize();
-    document.getElementById("num33").innerHTML +=  " = " + jsTpss.getUndoSize();
+    document.getElementById("num28").value =  jsTpss.hasTransactionToUndo();
+    document.getElementById("num29").value =  jsTpss.hasTransactionToRedo();
+    document.getElementById("num30").value =  numb.getNum();
+    document.getElementById("num31").value =  jsTpss.getSize();
+    document.getElementById("num32").value =  jsTpss.getRedoSize();
+    document.getElementById("num33").value =  jsTpss.getUndoSize();
 
 }   
 
