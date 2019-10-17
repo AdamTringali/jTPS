7e3e49043ec2d373512ccdc8d1b04272fff76a8e
AdamTringali <aatringali@gmail.com>
2019-10-17 12:15:02 -0400

    finish unit testing
    finish testRedo and testClear

--- jTPS-mine/Unit_Testing.js
+++ jTPS-mine/Unit_Testing.js
@@ -90,6 +90,139 @@ function add3Button(){
     document.getElementById("num31").value =  jsTpss.getSize();
     document.getElementById("num32").value =  jsTpss.getRedoSize();
     document.getElementById("num33").value =  jsTpss.getUndoSize();
-
 }   
+
+function testRedo() {
+    jsTpss = new jsTPS();
+    numb = new Num();
+
+    jsTpss.addTransaction(new AddToNum_Transaction(numb, 5));
+    jsTpss.addTransaction(new AddToNum_Transaction(numb, 10));
+    jsTpss.addTransaction(new AddToNum_Transaction(numb, 20));
+    document.getElementById("num34").value =  jsTpss.hasTransactionToUndo();
+    document.getElementById("num35").value =  jsTpss.hasTransactionToRedo();
+    document.getElementById("num36").value =  numb.getNum();
+    //document.getElementById("num37").value =  jsTpss.hasTransactionToUndo();
+    document.getElementById("num38").value =  jsTpss.getSize();
+    document.getElementById("num39").value =  jsTpss.getRedoSize();
+    document.getElementById("num40").value =  jsTpss.getUndoSize();
+
+    jsTpss.undoTransaction();
+    jsTpss.doTransaction();
+
+    document.getElementById("num41").value =  jsTpss.hasTransactionToUndo();
+    document.getElementById("num42").value =  jsTpss.hasTransactionToRedo();
+    document.getElementById("num43").value =  numb.getNum();
+    //document.getElementById("num44").value =  jsTpss.hasTransactionToUndo();
+    document.getElementById("num45").value =  jsTpss.getSize();
+    document.getElementById("num46").value =  jsTpss.getRedoSize();
+    document.getElementById("num47").value =  jsTpss.getUndoSize();
+
+    jsTpss.undoTransaction();
+    jsTpss.undoTransaction();
+    jsTpss.doTransaction();
+    jsTpss.doTransaction();
+
+    document.getElementById("num48").value =  jsTpss.hasTransactionToUndo();
+    document.getElementById("num49").value =  jsTpss.hasTransactionToRedo();
+    document.getElementById("num50").value =  numb.getNum();
+    //document.getElementById("num51").value =  jsTpss.hasTransactionToUndo();
+    document.getElementById("num52").value =  jsTpss.getSize();
+    document.getElementById("num53").value =  jsTpss.getRedoSize();
+    document.getElementById("num54").value =  jsTpss.getUndoSize();
+
+
+    jsTpss.undoTransaction();
+    jsTpss.undoTransaction();
+    jsTpss.undoTransaction();
+    jsTpss.doTransaction();
+    jsTpss.doTransaction();
+    jsTpss.doTransaction();
+
+    document.getElementById("num55").value =  jsTpss.hasTransactionToUndo();
+    document.getElementById("num56").value =  jsTpss.hasTransactionToRedo();
+    document.getElementById("num57").value =  numb.getNum();
+    //document.getElementById("num58").value =  jsTpss.hasTransactionToUndo();
+    document.getElementById("num59").value =  jsTpss.getSize();
+    document.getElementById("num60").value =  jsTpss.getRedoSize();
+    document.getElementById("num61").value =  jsTpss.getUndoSize();
+
+    jsTpss.undoTransaction();
+    jsTpss.undoTransaction();
+    jsTpss.undoTransaction();
+    jsTpss.doTransaction();
+    jsTpss.doTransaction();
+
+    document.getElementById("num62").value =  jsTpss.hasTransactionToUndo();
+    document.getElementById("num63").value =  jsTpss.hasTransactionToRedo();
+    document.getElementById("num64").value =  numb.getNum();
+    document.getElementById("num65").value =  jsTpss.getSize();
+    document.getElementById("num66").value =  jsTpss.getRedoSize();
+    document.getElementById("num67").value =  jsTpss.getUndoSize();
+
+    jsTpss.undoTransaction();
+    jsTpss.undoTransaction();
+    jsTpss.undoTransaction();
+    jsTpss.doTransaction();
+    jsTpss.doTransaction();
+    jsTpss.doTransaction();
+    jsTpss.doTransaction();
+
+    document.getElementById("num68").value =  jsTpss.hasTransactionToUndo();
+    document.getElementById("num69").value =  jsTpss.hasTransactionToRedo();
+    document.getElementById("num70").value =  numb.getNum();
+    document.getElementById("num71").value =  jsTpss.getSize();
+    document.getElementById("num72").value =  jsTpss.getRedoSize();
+    document.getElementById("num73").value =  jsTpss.getUndoSize();
+
+
+}
+
+function testClear() {
+    jsTpss = new jsTPS();
+    numb = new Num();
+    jsTpss.addTransaction(new AddToNum_Transaction(numb, 5));
+    jsTpss.addTransaction(new AddToNum_Transaction(numb, 10));
+    jsTpss.addTransaction(new AddToNum_Transaction(numb, 20));
+
+
+    document.getElementById("num74").value =  jsTpss.hasTransactionToUndo();
+    document.getElementById("num75").value =  jsTpss.hasTransactionToRedo();
+    document.getElementById("num76").value =  numb.getNum();
+    document.getElementById("num77").value =  jsTpss.getSize();
+    document.getElementById("num78").value =  jsTpss.getRedoSize();
+    document.getElementById("num79").value =  jsTpss.getUndoSize();
+
+    jsTpss.clearAllTransactions();
+
+    document.getElementById("num80").value =  numb.getNum();
+    document.getElementById("num81").value =  jsTpss.getSize();
+    document.getElementById("num82").value =  jsTpss.getRedoSize();
+    document.getElementById("num83").value =  jsTpss.getUndoSize();
+
+    jsTpss.addTransaction(new AddToNum_Transaction(numb, 5));
+    jsTpss.addTransaction(new AddToNum_Transaction(numb, 10));
+    jsTpss.addTransaction(new AddToNum_Transaction(numb, 20));
+
+    document.getElementById("num84").value =  numb.getNum();
+    document.getElementById("num85").value =  jsTpss.getSize();
+    document.getElementById("num86").value =  jsTpss.getRedoSize();
+    document.getElementById("num87").value =  jsTpss.getUndoSize();
+
+    jsTpss.clearAllTransactions();
+
+    document.getElementById("num88").value =  numb.getNum();
+    document.getElementById("num89").value =  jsTpss.getSize();
+    document.getElementById("num90").value =  jsTpss.getRedoSize();
+    document.getElementById("num91").value =  jsTpss.getUndoSize();
+
+    jsTpss.addTransaction(new AddToNum_Transaction(numb, 5));
+    jsTpss.addTransaction(new AddToNum_Transaction(numb, 10));
+    jsTpss.addTransaction(new AddToNum_Transaction(numb, 20));
+
+    document.getElementById("num92").value =  numb.getNum();
+    document.getElementById("num93").value =  jsTpss.getSize();
+    document.getElementById("num94").value =  jsTpss.getRedoSize();
+    document.getElementById("num95").value =  jsTpss.getUndoSize();
+}
 
